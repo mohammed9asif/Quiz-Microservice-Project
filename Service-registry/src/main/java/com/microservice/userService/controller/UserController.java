c5d24091286f0f843277d91b3976a420f78bf244
Mohammed Asif <asif21603@gmail.com>
2024-08-19 16:17:12 +0530

    add Service registry to the git

---
+++ Service-registry/src/main/java/com/microservice/userService/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.microservice.userService.controller;
+
+public class UserController {
+
+	
+	/* Api Design for User Service 
+	 *	register - create a new user and save in db
+	 *	login - to login with user , will use JWT
+	 *	profile/{id} - to get user profile
+	 *	profile/{id} - to update user profile */
+}
